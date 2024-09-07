9e7b5fd56dadcfc8dc8f6d2090c36c8a5e355f4f
Damian <hello@damianpetrov.com>
2024-09-07 13:09:47 +1000

    feat: env plugin

---
+++ netlify/plugins/inject-env.js
@@ -0,0 +1,14 @@
+module.exports = {
+    onPreBuild: ({ utils }) => {
+      const fs = require('fs');
+      const envContent = `
+        console.log('env.js loaded');
+        window.env = {
+          OPENWEATHER_API_KEY: '${process.env.OPENWEATHER_API_KEY}',
+          GOOGLE_MAPS_API_KEY: '${process.env.GOOGLE_MAPS_API_KEY}'
+        };
+      `;
+      fs.writeFileSync('src/env.js', envContent);
+      console.log('Injected environment variables into env.js');
+    },
+  };
--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,6 @@
-// src/js/config.js
+console.log('Config.js: window.env =', window.env);
+console.log('Config.js: OPENWEATHER_API_KEY =', window.env?.OPENWEATHER_API_KEY || 'not set');
+console.log('Config.js: GOOGLE_MAPS_API_KEY =', window.env?.GOOGLE_MAPS_API_KEY || 'not set');
+
 export const OPENWEATHER_API_KEY = window.env?.OPENWEATHER_API_KEY || '';
 export const GOOGLE_MAPS_API_KEY = window.env?.GOOGLE_MAPS_API_KEY || '';
